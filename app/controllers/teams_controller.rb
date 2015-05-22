836e79538c5eb54039761bb236ddf468d8b140e9
tiagofernandesp <tiagofernandesp@gmail.com>
2015-05-22 17:51:22 +0100

    Team controller concluido

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,10 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
+  def list
+      @teams = Team.find(:all)
+  end
+
   def create
     @team = Team.new team_params
 
@@ -13,11 +17,39 @@ class TeamsController < ApplicationController
     end
   end
 
+  def show
+      @team = Team.find(params[:id])
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+    @subjects = Subject.find(:all)
+  end
+
   private
 
-  def team_params
-    params.require(:team)
-      .permit(:name)
-      .merge(user: current_user)
-  end
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+     redirect_to :action => 'show', :id => @team
+   else
+     @subjects = Subject.find(:all)
+     render :action => 'edit'
+   end
+ end
+
+def delete
+  Team.find(params[:id]).destroy
+  /redirect_to :action => 'list'/
+end
+
+def show_subjects
+      @subject = Subject.find(params[:id])
+   end
+
+ def team_params
+  params.require(:team)
+  .permit(:name)
+  .merge(user: current_user)
+end
 end
